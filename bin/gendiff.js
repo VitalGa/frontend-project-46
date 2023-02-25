7846301bb29c159366d777916a6348aba3a9f537
VitalGa <vitalga10@gmail.com>
2023-02-25 16:09:03 +0300

    change

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,10 +3,10 @@
 import { Command } from 'commander/esm.mjs';
 const program = new Command();
 
-program
+ program
   .version('0.0.1', '-V, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
-program.parse();
+ program.parse();
      
 
     
